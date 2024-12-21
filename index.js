ff4ca38535f04f956f5c28f4a7dc7ce948ac168e
Azeddine-Mezouane <azeddine.mezouane@gmail.com>
2024-12-21 08:14:06 +0100

    js

---
+++ index.js
@@ -0,0 +1,21 @@
+db.students.insert( [   {
+    name: "Emma Johnson",
+    age: 20,
+    major: "Computer Science",
+    gpa: 3.8,
+    courses: ["Introduction to Programming", "Data Structures", "Algorithms"]
+  },
+  {
+    name: "Liam Smith",
+    age: 22,
+    major: "Mathematics",
+    gpa: 3.9,
+    courses: ["Calculus III", "Linear Algebra", "Number Theory"]
+  },
+  {
+    name: "Charlie Brown",
+    age: 21,
+    major: "Physics",
+    gpa: 3.9,
+    courses: ["Classical Mechanics", "Electromagnetism", "Quantum Physics"]
+  },])
